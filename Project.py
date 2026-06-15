01666fc826a5da83cbdea58b1b7b4a6a61e10557
MoyixP <victormvalls@gmail.com>
2026-06-15 13:32:41 +0200

    Analisis y comparativa de datos para validacion

---
+++ Project.py
@@ -0,0 +1,9 @@
+import pandas
+import pyomeca
+import os
+import numpy as np
+import matplotlib as plt
+import ezc3d as ez
+from pyomeca import Angles
+from pyomeca import Markers
+from pyomeca import Analogs
